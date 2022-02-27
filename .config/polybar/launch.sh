84b637c00951191c5e3f30aa4cbabc3953bf326f
hrog-zip <hrog-zip@gmail.com>
2022-02-27 12:16:53 +0300

    add polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload example &
+    done
+else
+    polybar --reload example &
+fi
+
+echo "Polybar launched..."
